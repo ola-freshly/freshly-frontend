73595e0efd34d2dce1a32b48b6c53b592d34f3fb
hieuvu121 <hieubeobilabi@gmail.com>
2026-07-24 20:51:29 +1000

    fix eslint

--- src/screens/mealplan/MealDetailScreen.tsx
+++ src/screens/mealplan/MealDetailScreen.tsx
@@ -60,6 +60,7 @@ export default function MealDetailScreen() {
   }, [recipeId]);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect -- fetch-on-mount; load() sets loading/error state by design
     void load();
   }, [load]);
 
